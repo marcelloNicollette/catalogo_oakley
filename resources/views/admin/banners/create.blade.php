9d905e089e5f7fd4500211a88ebcc58a19975bf8
Marcello Nicollette <marcello@wabawaba.com.br>
2025-10-03 11:02:58 -0300

    ajuste adm banners

--- resources/views/admin/banners/create.blade.php
+++ resources/views/admin/banners/create.blade.php
@@ -60,7 +60,7 @@
                 </div>
 
                 <div class="flex justify-end space-x-3">
-                    <a href="{{ route('admin.segmentacao.index') }}"
+                    <a href="{{ route('admin.banners.index') }}"
                         class="flex items-center bg-gray-100 hover:bg-gray-200 text-gray-700 font-semibold py-2 px-4 rounded-lg transition duration-150 ease-in-out">
                         <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg">
