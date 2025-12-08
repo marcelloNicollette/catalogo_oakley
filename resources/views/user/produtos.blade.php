e170f8a91d9e27cb8d99328649b3a8176c604924
Marcello Nicollette <marcello@wabawaba.com.br>
2025-12-08 12:58:48 -0300

    ajuste

--- resources/views/user/produtos.blade.php
+++ resources/views/user/produtos.blade.php
@@ -658,9 +658,9 @@
                                 <div class="option text-[18px]" data-slug="{{ $colecao->slug }}"
                                     data-value="{{ $colecao->slug }}"
                                     {{ $currentSlug == $colecao->slug ? 'selected' : '' }}
-                                    style=" {{ $currentSlug == $colecao->slug ? 'padding: 6px 15px 6px 1px;' : '' }}">
+                                    style=" {{ $currentSlug == $colecao->slug ? 'padding: 6px 0;' : '' }}">
                                     <span class="check-icon"
-                                        style="display: {{ $currentSlug == $colecao->slug ? 'inline; margin:0 10px;' : 'none' }};"><svg
+                                        style="display: {{ $currentSlug == $colecao->slug ? 'inline; margin:0 10px 0 0;' : 'none' }};"><svg
                                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
                                             <path
                                                 d="M530.8 134.1C545.1 144.5 548.3 164.5 537.9 178.8L281.9 530.8C276.4 538.4 267.9 543.1 258.5 543.9C249.1 544.7 240 541.2 233.4 534.6L105.4 406.6C92.9 394.1 92.9 373.8 105.4 361.3C117.9 348.8 138.2 348.8 150.7 361.3L252.2 462.8L486.2 141.1C496.6 126.8 516.6 123.6 530.9 134z" />
@@ -679,7 +679,8 @@
                                         <path
                                             d="M530.8 134.1C545.1 144.5 548.3 164.5 537.9 178.8L281.9 530.8C276.4 538.4 267.9 543.1 258.5 543.9C249.1 544.7 240 541.2 233.4 534.6L105.4 406.6C92.9 394.1 92.9 373.8 105.4 361.3C117.9 348.8 138.2 348.8 150.7 361.3L252.2 462.8L486.2 141.1C496.6 126.8 516.6 123.6 530.9 134z" />
                                     </svg></span>
-                                <span class="text-sm option-content">Todas</span>
+                                <span class="text-sm option-content"
+                                    style="margin: 0 5px 0 25px; font-size:16px">Todas</span>
                                 <span class="x-icon" style="display: none;">×</span>
                             </div>
                         </div>
