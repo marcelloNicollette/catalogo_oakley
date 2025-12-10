03bb4d521add4b50e36ffa6ce1f551fcff6b8dcc
Marcello Nicollette <marcello@wabawaba.com.br>
2025-12-10 12:31:12 -0300

    ajuste cms

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -203,7 +203,8 @@
                                             </div>
                                             <!-- Coluna 6: Numeração (por cor) -->
                                             <div>
-                                                <label class="block text-sm font-medium text-gray-700">Numeração</label>
+                                                <label
+                                                    class="block text-sm font-medium text-gray-700">Numeração/Tamanho</label>
                                                 <select :name="`color_numeracao_id[]`" x-model="campo.color_numeracao_id"
                                                     class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                                     <option value="">Selecione a Numeração</option>
