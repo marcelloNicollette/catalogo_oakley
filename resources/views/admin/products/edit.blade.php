63313e9645139306333ff497ec03cf4993132cac
Marcello Nicollette <marcello@wabawaba.com.br>
2026-01-09 17:19:52 -0300

    ajuste genero produtos

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -151,6 +151,7 @@
                                                 <label class="block text-sm font-medium text-gray-700">Gênero</label>
                                                 <select :name="`color_genero[]`" x-model="campo.color_genero"
                                                     class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                                                    <option value="Infantil">Infantil</option>
                                                     <option value="Masculino">Masculino</option>
                                                     <option value="Feminino">Feminino</option>
                                                     <option value="Unissex">Unissex</option>
